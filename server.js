35c0c709d47bf9c697f407fd14c9d7645d778235
haithemJegham <haithem.jegham@gmail.com>
2022-03-01 13:04:50 +0100

    for deploy

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express=require('express')
 const app=express()
+const path = require('path');
 
-app.listen(8000,()=>console.log('server is runnin on port 8000'))
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+app.listen(8000,()=>console.log('server is running on port 8000'))
