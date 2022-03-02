3ab3abbb57293de26139440942bf3b8ec2eab512
haithemJegham <haithem.jegham@gmail.com>
2022-03-02 17:31:10 +0100

    for deploy

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-const express=require('express')
-const app=express()
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
 
+const app = express();
 
-// ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+app.use(logger("dev"));
+app.use(express.json());
+app.use(favicon(path.join(dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(dirname, "build")));
+app.use("/api/users", require("./routes/api/users"));
 
+app.get("/*", function (req, res) {
+res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+const port = process.env.PORT || 3001;
 
-
-app.listen(8000,()=>console.log('server is running on port 8000'))
+app.listen(port, function () {
+console.log(`express app running on port ${port}`);
+});
